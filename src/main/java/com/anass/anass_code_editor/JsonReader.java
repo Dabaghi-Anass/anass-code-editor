08ddae8ac954792b4a7dbd99b773d1ee4381befe
Dabaghi-Anass <102183551+Dabaghi-Anass@users.noreply.github.com>
2023-09-04 17:22:27 +0100

    buuuugs

--- src/main/java/com/anass/anass_code_editor/JsonReader.java
+++ src/main/java/com/anass/anass_code_editor/JsonReader.java
@@ -1,4 +1,5 @@
 package com.anass.anass_code_editor;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.CollectionType;
 import com.fasterxml.jackson.databind.type.TypeFactory;
@@ -27,12 +28,34 @@ public class JsonReader<T> {
         }
     }
     public JsonReader(String filePath, Class<T> valueType,int count)  {
+        ObjectMapper objectMapper = new ObjectMapper();
         try {
             InputStream inputStream = JsonReader.class.getResourceAsStream(filePath);
             if (inputStream == null) return;
-            ObjectMapper objectMapper = new ObjectMapper();
             object = objectMapper.readValue(inputStream, valueType);
         } catch (Exception e) {
+            try {
+                object = objectMapper.readValue("""
+                                    {
+                                      "openedFolder": "",
+                                      "openedTabs": [],
+                                      "terminalSize": 1,
+                                      "codeAreaSize": 0.192,
+                                      "autoSave": false,
+                                      "intellisense": false,
+                                      "wrapText": false,
+                                      "minimized": false,
+                                      "selectedTab": "",
+                                      "windowWidth": 1025,
+                                      "windowHeight": 555,
+                                      "fontSize": 20,
+                                      "fontFamily": "Lucida Console",
+                                      "terminalMinimised": true,
+                                      "theme": "Default"
+                                    }""",valueType);
+            } catch (JsonProcessingException ex) {
+                System.out.println(ex.getMessage());
+            }
             System.out.println(e.getMessage());
         }
     }
